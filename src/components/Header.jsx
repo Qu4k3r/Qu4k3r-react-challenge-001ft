724533c8d493bd1ab36ba04d6233ff43d77ac5da
Qu4k3r <neves0007@gmail.com>
2021-05-20 23:07:36 -0300

    Install dependencies | Implement Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header>
+      <div className="header-container">
+        <div className="title-container">
+          <p className="main-title">Easily create or join a local nanny share with Hapu</p>
+          <p className="subtitle">
+            Hapu is Airbnb for nanny share.
+            Share your home, nanny and costs and create new flexible,
+            affordable solutions in childcare.
+          </p>
+          <div className="player">
+            <img src="/play-button.svg" alt="Play button" />
+            <p>See hapu in action (27 seconds)</p>
+          </div>
+        </div>
+        <div className="card-container">
+          <img src="/card.svg" alt="" />
+        </div>
+      </div>
+    </header>
+  );
+}
